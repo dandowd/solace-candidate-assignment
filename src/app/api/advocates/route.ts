b08c2c83d4725042f92fb99f711e90075a39f2c0
Dan Dowd <danieldowd89@gmail.com>
2025-09-23 18:45:01 -0600

    Removes seed route in favor of seed script
    Removes comment

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,8 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema/advocates";
-import { advocateData } from "../../../db/seed/advocates";
 
 export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
-
-  const data = advocateData;
+  const data = await db.select().from(advocates);
 
   return Response.json({ data });
 }

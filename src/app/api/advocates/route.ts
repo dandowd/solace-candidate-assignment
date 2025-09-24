2f184ab5141eb10eb8e220e841652f843948b384
Dan Dowd <danieldowd89@gmail.com>
2025-09-24 12:37:27 -0600

    Adds search to api

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,8 +1,23 @@
+import { NextRequest } from "next/server";
 import db from "../../../db";
 import { advocates } from "../../../db/schema/advocates";
+import { ilike } from "drizzle-orm";
 
-export async function GET() {
-  const data = await db.select().from(advocates);
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+
+  if (searchParams.has("search")) {
+    const searchTerm = searchParams.get("search");
+    const data = await db
+      .select()
+      .from(advocates)
+      .where(ilike(advocates.search,`%${searchTerm}%`))
+      .limit(10);
+      
+    return Response.json({ data });
+  }
+  // In a real system we would probably have a promoted flag and we would default to the promoted advocates for an area
+  const data = await db.select().from(advocates).limit(10);
 
   return Response.json({ data });
 }

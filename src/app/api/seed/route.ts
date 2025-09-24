131d26ca8c49b27ea139ac78fab2a8f87f66cab3
Dan Dowd <danieldowd89@gmail.com>
2025-09-23 18:02:14 -0600

    Creates schema folder
    This will help keep the file length low as the project grows
    
    Moves folder to db

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,5 +1,5 @@
 import db from "../../../db";
-import { advocates } from "../../../db/schema";
+import { advocates } from "../../../db/schema/advocates";
 import { advocateData } from "../../../db/seed/advocates";
 
 export async function POST() {
--- src/db/seed/advocates.ts
+++ src/db/seed/advocates.ts
@@ -1,5 +1,5 @@
 import db from "..";
-import { advocates } from "../schema";
+import { advocates } from "../schema/advocates";
 
 const specialties = [
   "Bipolar",

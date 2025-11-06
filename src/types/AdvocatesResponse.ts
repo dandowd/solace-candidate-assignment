41ede8d8e2ea4a2b6ac49ed91742056c93a0daef
Dan Dowd <danieldowd89@gmail.com>
2025-11-05 18:29:56 -0700

    Fixes advocate types

--- src/types/AdvocatesResponse.ts
+++ src/types/AdvocatesResponse.ts
@@ -1,7 +1,7 @@
 import { advocates } from "@/db/schema/advocates";
 import { InferSelectModel } from "drizzle-orm";
 
-type Specialty = { name: string; color: string };
+type SpecialtyResponse = { name: string; color: string };
 export type AdvocatesResponse = Array<
-  InferSelectModel<typeof advocates> & { specialties: Array<Specialty> }
+  InferSelectModel<typeof advocates> & { specialties: Array<SpecialtyResponse> }
 >;
